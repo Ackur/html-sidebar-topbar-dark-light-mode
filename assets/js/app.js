0f48553d57784ec2c9dcdbc6ca5e43e99fd4a491
vKremenok <vkremenok@lleida.net>
2025-06-15 14:35:45 +0200

    sidebar responsive logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,46 @@
 const elements = {
-  toggleSidebar: document.getElementById("toggle-sidebar"),
   sidebar: document.getElementById("app-sidebar"),
+  sidebarBackdrop: document.getElementById("app-sidebar-backdrop"),
+  sidebarToggle: document.getElementById("toggle-sidebar"),
 };
 
-function onToggleSidebarClicked() {
-  const expanded = this.getAttribute("aria-expanded") === "true";
+const sidebarWidth = {
+  expanded: getComputedStyle(document.documentElement).getPropertyValue(
+    "--sidebar-expanded-width"
+  ),
+  collapsed: getComputedStyle(document.documentElement).getPropertyValue(
+    "--sidebar-collapsed-width"
+  ),
+};
 
-  if (expanded) {
-    document.documentElement.style.setProperty("--sidebar-width", "80px");
-    this.setAttribute("aria-expanded", "false");
-    elements.sidebar.classList.add("expanded");
-  } else {
-    document.documentElement.style.setProperty("--sidebar-width", "250px");
-    this.setAttribute("aria-expanded", "true");
-    elements.sidebar.classList.remove("expanded");
-  }
+function expandSidebar() {
+  document.documentElement.style.setProperty(
+    "--sidebar-width",
+    sidebarWidth.expanded
+  );
+  elements.sidebarToggle.setAttribute("aria-expanded", "true");
+  elements.sidebar.classList.remove("collapsed");
 }
 
-elements.toggleSidebar.addEventListener("click", onToggleSidebarClicked);
+function collapseSidebar() {
+  document.documentElement.style.setProperty(
+    "--sidebar-width",
+    sidebarWidth.collapsed
+  );
+  elements.sidebarToggle.setAttribute("aria-expanded", "false");
+  elements.sidebar.classList.add("collapsed");
+}
+
+function toggleSidebar() {
+  const isExpanded =
+    elements.sidebarToggle.getAttribute("aria-expanded") === "true";
+  if (isExpanded) collapseSidebar();
+  else expandSidebar();
+}
+
+elements.sidebarToggle.addEventListener("click", toggleSidebar);
+elements.sidebarBackdrop.addEventListener("click", toggleSidebar);
+window.matchMedia("(min-width: 769px)").addEventListener("change", event => {
+  if (event.matches) expandSidebar();
+  else collapseSidebar();
+});
